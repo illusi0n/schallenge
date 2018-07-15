478c55f24fabe9395cdf555005d438b289624dfe
illusi0n <miloshb92@hotmail.com>
2018-07-15 14:37:57 +0200

    Added Swagger for API docs

--- src/main/java/com/schallenge/hotels/HotelController.java
+++ src/main/java/com/schallenge/hotels/HotelController.java
@@ -10,27 +10,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class HotelController {
 
 	@Autowired
 	private HotelService hotelService;
 
+	@ApiOperation(value="Get all hotels by name in ascending order")
 	@GetMapping("/hotels")
 	public List<Hotel> getAllAscending() {
 		return hotelService.getAllAscending();
 	}
 
+	@ApiOperation(value="Create new hotel by providing hotel details")
 	@PostMapping("/hotels")
 	public Hotel create(@RequestBody Hotel newHotel) {
 		return hotelService.create(newHotel);
 	}
 
+	@ApiOperation(value="Update hotel by providing updated details")
 	@PutMapping("/hotels")
-	public Hotel getAllById(@RequestBody Hotel updateHotel) {
+	public Hotel update(@RequestBody Hotel updateHotel) {
 		return hotelService.update(updateHotel);
 	}
 
+	@ApiOperation(value="Search hotel by name and address.Currently working only with both parameters.")
 	@GetMapping("/hotels/search")
 	public Hotel getByNameOrAddress(@RequestParam(required=false) String name, @RequestParam(required=false) String address) {
 		return hotelService.getByNameOrAddress(name, address);
