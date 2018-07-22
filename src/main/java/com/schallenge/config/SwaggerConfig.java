3eae834cdafbd3a920c64207fa4156d45f9070be
illusi0n <miloshb92@hotmail.com>
2018-07-22 23:20:29 +0200

    Added Account Controller to Swagger

--- src/main/java/com/schallenge/config/SwaggerConfig.java
+++ src/main/java/com/schallenge/config/SwaggerConfig.java
@@ -2,23 +2,22 @@ package com.schallenge.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
+
 	@Bean
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.schallenge.hotels"))
-				.paths(regex("/hotels.*")).build();
+				.apis(RequestHandlerSelectors.withClassAnnotation(RestController.class)).paths(PathSelectors.any())
+				.build();
 	}
-	
 }
